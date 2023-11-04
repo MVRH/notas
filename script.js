84f34e55ced504b2bbb0a9194eef95a8884a4523
MVRH <manuelvalenciarh@gmail.com>
2023-11-04 11:41:42 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
-// Load saved note from local storage when the page loads
+// Load saved note and font size from local storage when the page loads
 window.onload = function() {
     var savedNote = localStorage.getItem('savedNote');
     if (savedNote) {
         document.getElementById('noteArea').value = savedNote;
+    }
+
+    var savedFontSize = localStorage.getItem('fontSize');
+    if (savedFontSize) {
+        var fontSize = savedFontSize + 'px';
+        document.getElementById('noteArea').style.fontSize = fontSize;
+        document.getElementById('fontSizeSlider').value = savedFontSize;
     }
 };
 
 // Save note to local storage whenever text area content changes
 document.getElementById('noteArea').oninput = function() {
     localStorage.setItem('savedNote', this.value);
+};
+
+// Update font size when slider is moved and save it to local storage
+document.getElementById('fontSizeSlider').oninput = function() {
+    var fontSize = this.value + 'px';
+    document.getElementById('noteArea').style.fontSize = fontSize;
+    localStorage.setItem('fontSize', this.value);
 };

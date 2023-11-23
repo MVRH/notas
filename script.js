e6b51efc979df828877280f54995471fd153db57
MVRH <manuelvalenciarh@gmail.com>
2023-11-23 16:51:25 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,40 @@
-// Global variables to keep track of the current note, total notes, and delete mode status
+// Variables globales
 var currentNote = 1;
 var totalNotes = 1;
-var deleteMode = false;
+var linksActive = false;
 
-// Function to run once the page has loaded
+// Inicialización al cargar la página
 window.onload = function() {
- // Load total notes count from local storage
-    var savedTotalNotes = localStorage.getItem('totalNotes');
-    if (savedTotalNotes) {
-        totalNotes = parseInt(savedTotalNotes, 10);  // Parse the saved total notes count as an integer
-    }
-    
-    updateNoteButtons();  // Update the note buttons based on the current state
-    loadNote();  // Load the note for the current note number
-    loadFontSize();  // Load the saved font size
+    loadNote();
+    loadFontSize();
 
-    // Set up event listeners for the text area and font size slider
-    document.getElementById('noteArea').oninput = saveNote;  // Save note whenever text area content changes
-    document.getElementById('fontSizeSlider').oninput = updateFontSize;  // Update font size when slider is moved
-
-    // Set up event listeners to toggle delete mode when certain keys are pressed or released
-    window.addEventListener('keydown', toggleDeleteMode);
+    document.getElementById('noteArea').oninput = saveNote;
+    document.getElementById('fontSizeSlider').oninput = updateFontSize;
+    document.getElementById('linkButton').addEventListener('click', toggleLinks);
 };
+    document.getElementById('clearTextButton').addEventListener('click', clearText);
 
-// Function to toggle delete mode on or off based on key events
-function toggleDeleteMode(event) {
-console.log('toggleDeleteMode called', event.key, event.type);
-    if (event.key === 'Control' || event.key === 'Alt' || event.key === 'Meta' && event.type === 'keydown') {  // Check if the Alt key was pressed
-        deleteMode = !deleteMode;  // Toggle the deleteMode variable
-        updateNoteButtons();  // Update the note buttons to reflect the new delete mode state
-    }
+
+// Función para guardar la nota actual
+function saveNote() {
+    var noteContent = document.getElementById('noteArea').innerHTML;
+    localStorage.setItem('savedNote' + currentNote, noteContent);
 }
 
-// ... other functions remain unchanged ...
+// Función para cargar la nota
+function loadNote() {
+    var savedNote = localStorage.getItem('savedNote' + currentNote);
+    document.getElementById('noteArea').innerHTML = savedNote || '';
+}
+
+// Función para cargar el tamaño de fuente guardado
+function loadFontSize() {
+    var savedFontSize = localStorage.getItem('fontSize');
+    if (savedFontSize) {
+        document.getElementById('noteArea').style.fontSize = savedFontSize + 'px';
+        document.getElementById('fontSizeSlider').value = savedFontSize;
+    }
+}
 
 // Function to update the font size based on the slider value
 function updateFontSize() {
@@ -41,86 +43,38 @@ function updateFontSize() {
     localStorage.setItem('fontSize', this.value);  // Save the new font size to local storage
 }
 
-// Function to update the note buttons based on the current state
-function updateNoteButtons() {
-console.log('updateNoteButtons called', deleteMode);
-    var buttonContainer = document.getElementById('buttonContainer');
-    buttonContainer.innerHTML = '';  // Clear any existing buttons
-    
-    // Create a button for each note
-    for (var i = 1; i <= totalNotes; i++) {
-        var noteButton = document.createElement('button');
-        noteButton.textContent = i;
-        noteButton.onclick = function() { selectNote(this.textContent); };  // Set up click handler to select note
-        buttonContainer.appendChild(noteButton);  // Add the button to the container
-    }
-    
-    // Create the add/delete button
-    var addButton = document.createElement('button');
-    addButton.id = 'addButton';  // Assign an ID for CSS styling
-    if (deleteMode) {
-        addButton.textContent = '×';
-        addButton.className = 'delete-mode';
-        addButton.onclick = deleteLastNoteSlot;  // Set up click handler to delete the last note slot
+
+// Función para alternar los enlaces clickeables
+function toggleLinks() {
+    var noteArea = document.getElementById('noteArea');
+    var htmlContent = noteArea.innerHTML;
+    var linkButton = document.getElementById('linkButton');
+
+    if (!linksActive) {
+        // Convertir URL de texto en enlaces clicables
+        var urlRegex = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#/%?=~_|!:,.;]*[-A-Z0-9+&@#/%=~_|])|(\bwww\.[a-z0-9.-]+\.[a-z]{2,})|(\b[a-z0-9.-]+\.[a-z]{2,}(?:\/\S*)?)/ig;
+        htmlContent = htmlContent.replace(urlRegex, function(url) {
+            return '<a href="' + (url.startsWith('http') ? url : 'http://' + url) + '" target="_blank">' + url + '</a>';
+        });
+        noteArea.innerHTML = htmlContent;
+        linksActive = true;
+        linkButton.classList.add('active');
     } else {
-        addButton.textContent = '+';
-        addButton.className = 'add-button';
-        addButton.onclick = addNoteSlot;  // Set up click handler to add a new note slot
-    }
-    buttonContainer.appendChild(addButton);  // Add the button to the container
-}
-
-// Function to delete the last note slot
-function deleteLastNoteSlot() {
-console.log('deleteLastNoteSlot called', totalNotes);
-    if (totalNotes > 1) {  // Ensure there's more than one note slot
-        localStorage.removeItem('savedNote' + totalNotes);  // Remove the last note slot from local storage
-        totalNotes--;  // Decrement the total notes count
-        localStorage.setItem('totalNotes', totalNotes);  // Update total notes count in local storage
-        if (currentNote > totalNotes) {
-            currentNote = totalNotes;  // Update the current note number if necessary
-        }
-        updateNoteButtons();  // Update the note buttons
-        loadNote();  // Load the note for the new current note number
+        // Revertir enlaces clicables a texto
+        var links = noteArea.querySelectorAll('a');
+        links.forEach(function(link) {
+            noteArea.innerHTML = noteArea.innerHTML.replace(link.outerHTML, link.textContent);
+        });
+        linksActive = false;
+        linkButton.classList.remove('active');
     }
 }
 
-// Function to add a new note slot
-function addNoteSlot() {
-    totalNotes++;  // Increment the total notes count
-    localStorage.setItem('totalNotes', totalNotes);  // Update total notes count in local storage
-    updateNoteButtons();  // Update the note buttons
+// Función para limpiar el área de notas
+function clearText() {
+    var noteArea = document.getElementById('noteArea');
+    noteArea.innerHTML = ''; // Limpia el contenido
+    saveNote(); // Guarda el cambio en localStorage, si deseas mantener la nota vacía como el estado guardado
 }
 
-// Function to select a note
-function selectNote(noteNumber) {
-    saveNote();  // Save the current note
-    currentNote = noteNumber;  // Update the current note number
-    loadNote();  // Load the note for the new current note number
-}
 
-// Function to save the current note
-function saveNote() {
-    var noteContent = document.getElementById('noteArea').value;  // Get the current note content
-    localStorage.setItem('savedNote' + currentNote, noteContent);  // Save the current note to local storage
-}
-
-// Function to load the note for the current note number
-function loadNote() {
-    var savedNote = localStorage.getItem('savedNote' + currentNote);  // Get the saved note from local storage
-    if (savedNote) {
-        document.getElementById('noteArea').value = savedNote;  // Set the text area content to the saved note
-    } else {
-        document.getElementById('noteArea').value = '';  // Clear the text area if no saved note
-    }
-}
-
-// Function to load the saved font size
-function loadFontSize() {
-    var savedFontSize = localStorage.getItem('fontSize');  // Get the saved font size from local storage
-    if (savedFontSize) {
-        var fontSize = savedFontSize + 'px';  // Convert the saved font size to pixel units
-        document.getElementById('noteArea').style.fontSize = fontSize;  // Set the text area font size
-        document.getElementById('fontSizeSlider').value = savedFontSize;  // Set the slider value
-    }
-}
